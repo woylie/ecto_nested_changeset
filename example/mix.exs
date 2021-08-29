eddbcc60d15f824531343dcab30119d7623a702c
Mathias Polligkeit <git@woylie.de>
2021-08-29 11:38:39 +0900

    update deps

--- example/mix.exs
+++ example/mix.exs
@@ -33,7 +33,7 @@ defmodule Nested.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.12"},
+      {:phoenix, "~> 1.6.0-rc.0", override: true},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.7"},
       {:postgrex, ">= 0.0.0"},
