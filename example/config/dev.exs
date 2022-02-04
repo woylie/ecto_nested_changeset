b4d42f26fb30f4fdcb541bea55813430d73f49bb
Jack Fowler <jackmihalik@icloud.com>
2022-02-04 12:08:41 +0900

    Fix files

--- example/config/dev.exs
+++ example/config/dev.exs
@@ -18,7 +18,7 @@ config :nested, Nested.Repo,
 config :nested, NestedWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4010],
+  http: [ip: {127, 0, 0, 1}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
