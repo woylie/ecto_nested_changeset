33c80a74a0d8f2d6dadd04f33cf28945204d3490
Mathias Polligkeit <git@woylie.de>
2022-11-07 21:45:31 +0900

    remove unused functions

--- example/lib/nested/members.ex
+++ example/lib/nested/members.ex
@@ -4,9 +4,9 @@ defmodule Nested.Members do
   """
 
   import Ecto.Query, warn: false
-  alias Nested.Repo
 
   alias Nested.Members.Owner
+  alias Nested.Repo
 
   @doc """
   Returns the list of owners.
@@ -37,8 +37,12 @@ defmodule Nested.Members do
       ** (Ecto.NoResultsError)
 
   """
-  def get_owner!(id),
-    do: Owner |> where(id: ^id) |> preload(pets: [:toys]) |> Repo.one!()
+  def get_owner!(id) do
+    Owner
+    |> where(id: ^id)
+    |> preload(pets: [:toys])
+    |> Repo.one!()
+  end
 
   @doc """
   Creates a owner.
@@ -104,197 +108,5 @@ defmodule Nested.Members do
   """
   def change_owner(%Owner{} = owner, attrs \\ %{}) do
     Owner.changeset(owner, attrs)
-  end
-
-  alias Nested.Members.Pet
-
-  @doc """
-  Returns the list of pets.
-
-  ## Examples
-
-      iex> list_pets()
-      [%Pet{}, ...]
-
-  """
-  def list_pets do
-    Repo.all(Pet)
-  end
-
-  @doc """
-  Gets a single pet.
-
-  Raises `Ecto.NoResultsError` if the Pet does not exist.
-
-  ## Examples
-
-      iex> get_pet!(123)
-      %Pet{}
-
-      iex> get_pet!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_pet!(id), do: Repo.get!(Pet, id)
-
-  @doc """
-  Creates a pet.
-
-  ## Examples
-
-      iex> create_pet(%{field: value})
-      {:ok, %Pet{}}
-
-      iex> create_pet(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_pet(attrs \\ %{}) do
-    %Pet{}
-    |> Pet.changeset(attrs)
-    |> Repo.insert()
-  end
-
-  @doc """
-  Updates a pet.
-
-  ## Examples
-
-      iex> update_pet(pet, %{field: new_value})
-      {:ok, %Pet{}}
-
-      iex> update_pet(pet, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_pet(%Pet{} = pet, attrs) do
-    pet
-    |> Pet.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a pet.
-
-  ## Examples
-
-      iex> delete_pet(pet)
-      {:ok, %Pet{}}
-
-      iex> delete_pet(pet)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_pet(%Pet{} = pet) do
-    Repo.delete(pet)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking pet changes.
-
-  ## Examples
-
-      iex> change_pet(pet)
-      %Ecto.Changeset{data: %Pet{}}
-
-  """
-  def change_pet(%Pet{} = pet, attrs \\ %{}) do
-    Pet.changeset(pet, attrs)
-  end
-
-  alias Nested.Members.Toy
-
-  @doc """
-  Returns the list of toys.
-
-  ## Examples
-
-      iex> list_toys()
-      [%Toy{}, ...]
-
-  """
-  def list_toys do
-    Repo.all(Toy)
-  end
-
-  @doc """
-  Gets a single toy.
-
-  Raises `Ecto.NoResultsError` if the Toy does not exist.
-
-  ## Examples
-
-      iex> get_toy!(123)
-      %Toy{}
-
-      iex> get_toy!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_toy!(id), do: Repo.get!(Toy, id)
-
-  @doc """
-  Creates a toy.
-
-  ## Examples
-
-      iex> create_toy(%{field: value})
-      {:ok, %Toy{}}
-
-      iex> create_toy(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_toy(attrs \\ %{}) do
-    %Toy{}
-    |> Toy.changeset(attrs)
-    |> Repo.insert()
-  end
-
-  @doc """
-  Updates a toy.
-
-  ## Examples
-
-      iex> update_toy(toy, %{field: new_value})
-      {:ok, %Toy{}}
-
-      iex> update_toy(toy, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_toy(%Toy{} = toy, attrs) do
-    toy
-    |> Toy.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a toy.
-
-  ## Examples
-
-      iex> delete_toy(toy)
-      {:ok, %Toy{}}
-
-      iex> delete_toy(toy)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_toy(%Toy{} = toy) do
-    Repo.delete(toy)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking toy changes.
-
-  ## Examples
-
-      iex> change_toy(toy)
-      %Ecto.Changeset{data: %Toy{}}
-
-  """
-  def change_toy(%Toy{} = toy, attrs \\ %{}) do
-    Toy.changeset(toy, attrs)
   end
 end
