8b1a8a3b11f1aa48865e6df132d4a8b9e085841b
Mathias Polligkeit <git@woylie.de>
2023-12-23 18:09:13 +0900

    update dependencies

--- example/mix.exs
+++ example/mix.exs
@@ -40,7 +40,7 @@ defmodule Nested.MixProject do
       {:jason, "~> 1.2"},
       {:phoenix, "~> 1.7.0"},
       {:phoenix_ecto, "~> 4.4"},
-      {:phoenix_html, "~> 3.3"},
+      {:phoenix_html, "~> 4.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.20.0"},
       {:plug_cowboy, "~> 2.5"},
